c0cb696bc7db306e3802e8939b05fbd611863a6d
User <you@example.com>
2025-04-06 19:43:27 -0400

    port numbers fixed

--- playwright.config.js
+++ playwright.config.js
@@ -12,6 +12,9 @@ import { defineConfig, devices } from '@playwright/test'
 /**
  * @see https://playwright.dev/docs/test-configuration
  */
+
+const PORT = 8080
+
 export default defineConfig({
   testDir: './e2e-tests',
   /* Run tests in files in parallel */
@@ -27,7 +30,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:5000',
+    baseURL: `http://localhost:${PORT}`,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -74,7 +77,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'npm run start',
-    url: 'http://localhost:5000',
+    url: `http://localhost:${PORT}`,
     reuseExistingServer: !process.env.CI,
     // timeout: 120 * 1000,
   },
