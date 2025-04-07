b934fe52c074f69d59ce85d60405ddf7863848f9
User <you@example.com>
2025-04-07 15:15:01 -0400

    added navigate page test

--- e2e-tests/pokedex.spec.js
+++ e2e-tests/pokedex.spec.js
@@ -1,10 +1,18 @@
-// eslint-disable-next-line no-unused-vars
 const { test, describe, expect, beforeEach } = require('@playwright/test')
 
 describe('Pokedex', () => {
-  test('front page can be opened', async ({ page }) => {
+  beforeEach(async ({ page }) => {
     await page.goto('')
+  })
+
+  test('front page can be opened', async ({ page }) => {
+    // await page.goto('')
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
+  })
+
+  test('pokemon page can be navigated to', async ({ page }) => {
+    await page.getByRole('link', { name: 'ivysaur' }).click()
+    await expect(page.getByText('chlorophyll')).toBeVisible()
   })
 })
